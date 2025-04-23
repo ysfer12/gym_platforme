edb6a6ab68197b83b6977aef1c57fd787fee0402
ysfer12 <youssefrouihi90@gmail.com>
2025-04-23 18:26:49 +0200

    add member mobile side bar

--- project/resources/views/partials/mobile-menu.blade.php
+++ project/resources/views/partials/mobile-menu.blade.php
@@ -51,11 +51,11 @@
                     Attendance
                 </a>
 
-                <a href="#" class="{{ request()->routeIs('member.profile') ? 'bg-indigo-50 text-indigo-600' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }} group flex items-center px-3 py-3 text-base font-medium rounded-md">
+                <a href="{{ route('member.profile') }}" class="{{ request()->routeIs('member.profile') ? 'bg-indigo-50 text-indigo-600' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }} group flex items-center px-3 py-3 text-base font-medium rounded-md">
                     <svg class="mr-4 flex-shrink-0 h-6 w-6 {{ request()->routeIs('member.profile') ? 'text-indigo-600' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
-                    Profile
+                    Profile & Badge
                 </a>
 
                 <form method="POST" action="{{ route('logout') }}" class="mt-6">
