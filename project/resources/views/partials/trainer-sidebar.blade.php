0b42fbeae2a86b5e65c71d77fe2579cbd3653ec3
ysfer12 <youssefrouihi90@gmail.com>
2025-04-25 10:17:55 +0200

    update trainer mobile menu

--- project/resources/views/partials/trainer-sidebar.blade.php
+++ project/resources/views/partials/trainer-sidebar.blade.php
@@ -42,13 +42,13 @@
                         Members
                     </a>
 
-                    <!-- Profile Link -->
-                    <a href="#" class="{{ request()->routeIs('trainer.profile*') ? 'bg-indigo-50 text-indigo-600' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }} group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                        <svg class="mr-3 flex-shrink-0 h-6 w-6 {{ request()->routeIs('trainer.profile*') ? 'text-indigo-600' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                        </svg>
-                        Profile
-                    </a>
+<!-- Profile Link -->
+<a href="{{ route('trainer.profile') }}" class="{{ request()->routeIs('trainer.profile*') ? 'bg-indigo-50 text-indigo-600' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' }} group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+    <svg class="mr-3 flex-shrink-0 h-6 w-6 {{ request()->routeIs('trainer.profile*') ? 'text-indigo-600' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+    </svg>
+    Profile
+</a>
                 </nav>
             </div>
         </div>
